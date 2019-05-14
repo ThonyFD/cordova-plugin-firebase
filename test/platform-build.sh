9bd9f19f3d99b257158ecb10edede22f6aa078fe
Anthony F. Mosquera G <fdsystems.fd@gmail.com>
2019-05-14 11:53:39 -0500

    Update platform-build.sh

--- test/platform-build.sh
+++ test/platform-build.sh
@@ -9,5 +9,4 @@ PLATFORM_VERSION=$3
 FOLDER=".build-$PLATFORM"
 cd $FOLDER
 
-../node_modules/.bin/cordova build $PLATFORM --debug --verbose -- --stacktrace
 ../.build-android/platforms/android/gradlew build --stacktrace
